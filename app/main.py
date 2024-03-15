6c6bf85207bdb17e6f3bf1eb8109bc205b3dcb44
Kenny Rachuonyo <krmboya@gmail.com>
2024-03-15 07:01:41 +0300

    Do not recv from socket via loop

--- app/main.py
+++ app/main.py
@@ -10,17 +10,12 @@ def main():
     client_socket, client_address = server_socket.accept()  # accept connection
     print(f"Received client connection via {client_address}")
     # read data from socket
-    chunks = []
-    while True:
-        msg = client_socket.recv(4096)
-        if not msg:
-            break
-        chunks.append(msg)
-    print(f"message received: {chunks}")
+    msg = client_socket.recv(4096)
+    print(f"message received: {msg}")
     # write to socket
     response_text = "HTTP/1.1 200 OK\r\n\r\n"
     client_socket.sendall(bytes(response_text, "utf-8"))
-    # close socket - necessary?
+    client_socket.close()
 
 
 if __name__ == "__main__":
