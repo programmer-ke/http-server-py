29da55e27d2eb7abcd6a45e69ca076db9ec9e91d
Kenny Rachuonyo <krmboya@gmail.com>
2024-03-15 06:40:52 +0300

    read and write client socket

--- app/main.py
+++ app/main.py
@@ -7,7 +7,20 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    client_socket, client_address = server_socket.accept()  # accept connection
+    print(f"Received client connection via {client_address}")
+    # read data from socket
+    chunks = []
+    while True:
+        msg = client_socket.recv(4096)
+        if not msg:
+            break
+        chunks.append(msg)
+    print(f"message received: {chunks}")
+    # write to socket
+    response_text = "HTTP/1.1 200 OK\r\n\r\n"
+    client_socket.sendall(bytes(response_text))
+    # close socket - necessary?
 
 
 if __name__ == "__main__":
